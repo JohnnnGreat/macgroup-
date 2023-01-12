d27f81db4cf37c945279f102ecb663bd28dfe057
John Ossai <johnossai20@gmai.com>
2023-01-11 18:52:04 -0800

    New File

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.scss";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <div className="header-logo">
+          <h1 className="logo-text"> MACGROUP</h1>
+        </div>
+        <div className="header-navBtn"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
