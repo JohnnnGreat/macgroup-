ab210b5bc3b05ecb6cac38d36bc293b3188c7875
Dlite <afekhide009@gmail.com>
2023-01-13 03:20:06 +0100

    Added Next Section, After Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header.js";
 import Hero from "./Components/Hero/Hero.js";
+import SalesPitch from "./Components/SalesPitch/SalesPitch";
+
 function App() {
   return (
     <div className="App">
       <Header title={"MACGROUP"} sub={"INTERNATIONAL TECH SERVICE"} />
       <Hero />
+      <SalesPitch/>
     </div>
   );
 }
