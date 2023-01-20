2392e8c1a2c54490b8eb50cde6a00fe398b5c7d3
John Ossai <johnossai20@gmai.com>
2023-01-19 21:43:19 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/services" element={<ServicePage />}></Route>
         </Routes>
       </div>
-      {/* <Footer /> */}{" "}
+      <Footer />
     </BrowserRouter>
   );
 }
