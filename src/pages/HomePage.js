0d6787594c8eeee4cf7d8dd6813fad924c4771d8
John Ossai <johnossai20@gmai.com>
2023-02-20 06:45:44 -0800

    New files

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,11 +5,8 @@ import Brand from "../Components/Brand/Brand.js";
 import Brief from "../Components/Brief/Brief.js";
 import Team from "../Components/Team/Team.js";
 import Values from "../Components/Values/Values.jsx";
-<<<<<<< HEAD
 import Companies from "../Components/Companies/Companies.js";
 import Projects from "../Components/Projects/Projects.js";
-=======
->>>>>>> 29c2a7384a93544ff2c189ae13ca882a283bafb7
 import AOS from "aos";
 import { animateScroll as scroll } from "react-scroll";
 // import SalesPitch from "../Components/SalesPitch/SalesPitch.js";
@@ -25,17 +22,11 @@ function HomePage() {
   return (
     <>
       <Hero />
-<<<<<<< HEAD
       <Companies />
       <Websites />
       <Brand />
       <Brief />
       <Projects />
-=======
-      <Websites />
-      <Brand />
-      <Brief />
->>>>>>> 29c2a7384a93544ff2c189ae13ca882a283bafb7
       <Values />
       <Team />
       <Footer />
